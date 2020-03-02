754ecc1c7fb0efce8ae4d9f4ee4be762e0e3ed55
Francisco Andaur <francisco.andaur@icloud.com>
2020-03-02 22:56:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,30 +4,6 @@ function destructivelyAppendKitten(n){
   return kittens.push(n)
 }
 
-function destructivelyPrependKitten(n){
-  return kittens.unshift(n)
-}
-
-function destructivelyRemoveLastKitten(){
-  return kittens.pop()
-}
-
-function destructivelyRemoveFirstKitten() {
-  return kittens.shift()
-}
-
-function appendKitten(n){
-  return [...kittens, n]
-}
-
-function prependKitten(n){
-  return [n, ...kittens]
-}
-
-function removeLastKitten(){
-  return kittens.slice(0, kittens.length-1)
-}
-
-function removeFirstKitten(){
-  return kittens.slice(1)
+function destructivelyPrependKitten(n) {
+  return kittens.pop(-1);
 }
